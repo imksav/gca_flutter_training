93c8a8c708e74b487e6ba3389cf8f7ae60f3cb35
Keshav Bhandari <imksav@gmail.com>
2020-12-11 17:35:34 +0545

    learning gesture part 1

--- day1/lib/screens/home.dart
+++ day1/lib/screens/home.dart
@@ -1,69 +1,100 @@
 import 'package:flutter/material.dart';
 
+/*
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text("This is title - ImKsav"),
-        actions: [
-          Icon(Icons.notification_important),
-          SizedBox(width: 20),
-          Icon(Icons.videocam),
-        ],
-        // centerTitle: true,
+        title: Text("Title"),
       ),
-      body: ListView(
-        // scrollDirection: Axis.horizontal,
-        // crossAxisAlignment: CrossAxisAlignment.start,
+      body: Text("data"),
+    );
+  }
+}
+*/
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String changeValue = "Initial";
+  String gestureValue = "Changing value using gesture";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("1"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Container(height: 100, width: 100, color: Colors.grey),
-          SizedBox(height: 10),
-          Container(height: 100, width: 100, color: Colors.blue),
-          Container(height: 100, width: 100, color: Colors.grey),
-          Text("This is text widget", style: TextStyle(color: Colors.red)),
-          TextField(),
-          RaisedButton(
-              color: Colors.amber,
-              elevation: 5,
-              onPressed: () {
-                print("tel:9823872337");
-              },
-              child: Text("Raised Button")),
-          FlatButton(
-              color: Colors.grey,
-              onPressed: () {
-                print("Pressed");
-              },
-              child: Text("FlatButton")),
-          Image.network(
-            "https://course.genesecloud.academy/pluginfile.php?file=%2F1%2Ftheme_alpha%2Fcustomlogotopbar%2F1606229507%2Fsmall-icon.png",
-            height: 40,
-            width: 40,
-          ),
-          Container(
-            color: Colors.red,
-            height: 200,
-            width: 200,
-            child: Stack(
-              children: [
-                Container(color: Colors.amber, height: 200, width: 200),
-                Container(color: Colors.green, height: 100, width: 100),
-                Text("I am at the top."),
-              ],
+          GestureDetector(
+            onTap: () {
+              print("Working");
+              setState(() {
+                gestureValue = "OnTap Called";
+              });
+            },
+            onLongPress: () {
+              setState(() {
+                gestureValue = "OnLong Preess";
+              });
+            },
+            onDoubleTap: () {
+              setState(() {
+                gestureValue = "On Double Tap";
+              });
+            },
+            child: Text(
+              "Provide gesture here",
+              style: TextStyle(fontSize: 20),
             ),
           ),
-          Container(height: 100, width: 100, color: Colors.grey),
-          Container(height: 100, width: 100, color: Colors.blue),
-          Container(height: 100, width: 100, color: Colors.grey),
-          Text("This is text widget", style: TextStyle(color: Colors.red)),
-          Container(
-            height: 200,
-            width: 100,
-            color: Color.fromRGBO(255, 155, 10, 10),
-          )
+          RaisedButton(
+            onPressed: () {
+              changeValue = "changed";
+              print(changeValue);
+              setState(() {});
+            },
+            child: Text(" Me"),
+          ),
+          Row(
+            children: [
+              Text("Gesture Status:\t\t\t"),
+              Text(
+                gestureValue,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  String changeValue = "Initial value";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Change"),
+      ),
+      body: Column(
+        children: [
+          Text(changeValue),
+          RaisedButton(
+            onPressed: () {
+              changeValue = "changed";
+              print(changeValue);
+            },
+            child: Text("Click Me"),
+          ),
         ],
       ),
     );
