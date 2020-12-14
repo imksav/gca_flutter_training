4ac81725917c7bcac0e9a44e90f0bc4604f9a328
Keshav Bhandari <imksav@gmail.com>
2020-12-14 18:40:35 +0545

    Completed function

---
+++ day1/lib/dart/class.dart
@@ -0,0 +1,7 @@
+void main() {}
+
+class Student {
+  int id;
+  String name;
+  int age;
+}
--- day1/lib/dart/functions.dart
+++ day1/lib/dart/functions.dart
@@ -2,7 +2,20 @@ void main() {
   display(1, "Keshav Bhandari");
   displayNamed(id: 1, name: "Keshav Bhandari");
   displayOptional("keshav bhandari");
-  displayPositionalAndNamed();
+  displayPositionalAndNamed("orange", price: 200);
+  double price = calculate(price: 99);
+  print(price);
+  print(getUser());
+}
+
+Map<String, dynamic> getUser() {
+  return {"id": 1, 'name': "keshav Bhandari"};
+}
+
+double calculate({double price}) {
+  double incrAmount = 9.99;
+  double newAmount = price + 9.99;
+  return newAmount;
 }
 
 void display(int id, String name) {
@@ -17,4 +30,6 @@ void displayOptional([String name, int id]) {
   print("My name is $name. My id is $id");
 }
 
-void displayPositionalAndNamed() {}
+void displayPositionalAndNamed(String itemName, {double price}) {
+  print("Price of $itemName is Rs.$price.");
+}
