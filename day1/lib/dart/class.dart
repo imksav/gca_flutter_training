9edf0580f7a391219a3328e7fbdc0efba9672c45
Keshav Bhandari <imksav@gmail.com>
2020-12-14 18:51:25 +0545

    learning classes

--- day1/lib/dart/class.dart
+++ day1/lib/dart/class.dart
@@ -1,7 +1,24 @@
-void main() {}
+void main() {
+  Student s1 = new Student();
+  s1.display();
+  Student.init();
+}
 
 class Student {
-  int id;
-  String name;
-  int age;
+  void display() {
+    print("From class Students.");
+  }
+
+  static double rate;
+  static double init() {
+    print("Inatilazing......");
+    rate = 5.99;
+    _finish();
+    return rate;
+  }
+
+  static void _finish() {
+    print("Finished....");
+    print(rate);
+  }
 }
